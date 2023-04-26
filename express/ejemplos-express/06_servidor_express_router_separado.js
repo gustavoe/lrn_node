b95f187e54983e03b7748dde4818e6e1b0660ba7
Gustavo Echeverria <gustavo.echeverria@gmail.com>
2023-04-26 11:45:29 -0300

    Router separado en express

---
+++ express/ejemplos-express/06_servidor_express_router_separado.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { infoCursos } = require('./datos/cursos.js');
+
+const PORT = process.env.port || 3000;
+
+const app = express();
+
+// Routers
+const routerProgramacion = require('./routers/programacion.js');
+const routerMatematicas = require('./routers/matematicas.js');
+
+app.use('/api/cursos/programacion', routerProgramacion);
+app.use('/api/cursos/matematicas', routerMatematicas);
+
+
+// Routing
+
+app.get('/', (req, res) => {
+  res.send('<h1>Servidor Express Home</h1>')
+});
+
+app.get('/api/cursos', (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+})
+
+
+app.listen(PORT, () =>{
+  console.log(`Escuchando en http://localhost:${PORT}`);
+})
